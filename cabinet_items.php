0ea11f5141660bfe96e56f09fa7800f9cbec1c89
Justin Granados Campos <justingranados34@gmail.com>
2021-03-28 15:59:38 -0600

    ...

--- cabinet_items.php
+++ cabinet_items.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Items ֎ Cabinets</title>
+    <title>Items in Cabinets</title>
     <?php
         include './styles.php';
     ?>
--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Items ֎ Boxes</title>
+    <title>Items in Boxes</title>
     <?php
         include './styles.php';
     ?>
